de5d26b14bb5ce18044f57a09649fd2e594f7bf1
Guadacabr <guada.9819gmail.com>
2022-06-22 00:49:33 -0300

    Subiendo Cadenas1

---
+++ Cadenas1/2cadenas.py
@@ -0,0 +1,5 @@
+nombre = input ("¿Cómo te llamas? ")
+print(nombre.lower())
+print(nombre.upper())
+print(nombre.title())
+
---
+++ Cadenas1/4cadenas.py
@@ -0,0 +1,2 @@
+tel = input("Ingresa un número de télefono con el formato +xx-xxxxxxxxx-xx: ")
+print("El número del télefono es ", tel[4:-3])
